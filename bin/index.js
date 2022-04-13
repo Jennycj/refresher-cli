574aa4796bdb3a7c34f0a241182dfc3c8788d5ab
Jennycj <ezeobijennifer2@gmail.com>
2022-04-13 15:14:46 +0100

    add connection to bitcoind

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
-import cli from "commander";
+const cli = require("commander");
+const pkg = require("../package.json")
 
-cli.description("An interface to create timelocked pswsh transactions and refresh the timelock");
-cli.name("refresher-cli");
-cli.usage("<command>");
+cli
+ .description("An interface to create timelocked pswsh transactions and refresh the timelock")
+ .name("refresher-cli")
+ .usage("<command>")
+ .version(pkg.version)
+ .parse(process.argv);
 
-cli.parse(process.argv);
+console.log("Hello fron here!");
